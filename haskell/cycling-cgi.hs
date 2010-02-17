95d8981f9d505b83a55fa7381831246088766344
Rohan Drape <rd@slavepianos.org>
2010-02-17 06:01:30 +0000

    rename f -> get_f

--- haskell/cycling-cgi.hs
+++ haskell/cycling-cgi.hs
@@ -13,25 +13,25 @@ type State = ()
 
 dispatch :: State -> String -> [(String,String)] -> CGI CGIResult
 dispatch _ _ st =
-    let f df nm = read (fromMaybe df (lookup nm st))
+    let get_f df nm = read (fromMaybe df (lookup nm st))
         chart = fromMaybe "gearing" (lookup "chart" st)
         o = case chart of
               "cadence" -> 
-                  let c = f "60.0" "cadence"
+                  let c = get_f "60.0" "cadence"
                   in C.mk_gearing_chart (C.mk_cadence c)
               "gearing" -> 
-                  let c_min = f "60.0" "cadence-minima"
-                      c_max = f "110.0" "cadence-maxima"
-                      v = f "36.0" "velocity"
+                  let c_min = get_f "60.0" "cadence-minima"
+                      c_max = get_f "110.0" "cadence-maxima"
+                      v = get_f "36.0" "velocity"
                       g = (c_min, c_max, v)
                   in C.mk_gearing_chart (C.mk_gearing g)
               "gradient" -> 
-                  let t = f "0.05" "tolerance"
-                      m_r = f "62.0" "rider-weight"
-                      m_b = f "8.0" "bicycle-weight"
-                      m_k = f "2.0" "kit-weight"
+                  let t = get_f "0.05" "tolerance"
+                      m_r = get_f "62.0" "rider-weight"
+                      m_b = get_f "8.0" "bicycle-weight"
+                      m_k = get_f "2.0" "kit-weight"
                       m = m_r + m_b + m_k
-                      w = f "250.0" "power"
+                      w = get_f "250.0" "power"
                   in C.mk_gradient_chart (C.mk_gradient (t, m, w))
               _ -> undefined
     in utf8_output o
