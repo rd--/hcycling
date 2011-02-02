06e5d573c88e8c8a7a4de89c2d200232393b6674
Rohan Drape <rd@slavepianos.org>
2011-02-02 08:46:15 +0000

    make parse_hms more lenient

--- haskell/Time.hs
+++ haskell/Time.hs
@@ -8,4 +8,6 @@ parse_hms x =
     case S.splitOneOf ":." x of
       [h,m,s,ms] -> (read h,read m,read s,read ms)
       [m,s,ms] -> (0,read m,read s,read ms)
+      [m,s] -> (0,read m,read s,0)
+      [m] -> (0,read m,0,0)
       _ -> error "parse_hms"
