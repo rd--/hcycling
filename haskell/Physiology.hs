2633a176f9740376106521fbb73e7d5481ecbc79
Rohan Drape <rd@slavepianos.org>
2011-03-15 05:16:34 +0000

    Basal metabolic rate (Physiology)

--- haskell/Physiology.hs
+++ haskell/Physiology.hs
@@ -1,5 +1,7 @@
 module Physiology where
 
+-- * Age predicted heart rate maxima.
+
 type HR_MAX = Double->Double
 
 hr_max_haskell_and_fox :: HR_MAX
@@ -33,6 +35,8 @@ hr_max_all =
     ,hr_max_oakland_nonlinear
     ,hr_max_lund]
 
+-- * Heart rate training zones
+
 -- http://www8.garmin.com/manuals/Edge500_OwnersManual.pdf
 hr_zones_garmin :: (Fractional a) => a -> [(a,a)]
 hr_zones_garmin hr_max =
@@ -62,3 +66,39 @@ hr_zones_zoladz 184
 map (hr_target_karvonen 54 184) [0.5,0.85]
 -}
 
+-- * Basal metabolic rate
+
+type R = Double
+
+cal_th_to_joules :: R
+cal_th_to_joules = 4.184
+
+-- Dr. J.A. Harris and Dr. F.G. Benedict (closed circuit spirometry)
+bmr_harris_benedict_1919_men :: R -> R -> R -> R
+bmr_harris_benedict_1919_men w h a =
+    let w' = 13.7516 * w
+        h' = 5.0033 * h
+        a' = 6.7550 * a
+    in 66.473 + w' + h' + a'
+
+{-
+bmr_harris_benedict_1919_men 63 176 35
+=> 2050
+-}
+
+data Gender = Male | Female
+
+bmr_mifflin_1990 :: (Fractional t) => t -> t -> t -> Gender -> t
+bmr_mifflin_1990 w h a g =
+    let w' = 10 * w
+        h' = 6.25 * h
+        a' = 5 * a
+        g' = case g of
+               Male -> 5
+               Female -> (-161)
+    in w' + h' + a' + g'
+
+{-
+bmr_mifflin_1990 63 176 35 Male
+=> 1910
+-}
